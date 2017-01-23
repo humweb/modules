97c4cd333d33ce5604493a7581a81d140c6e8b35
Ryan Shofner <ryan@letsgolearn.com>
2017-01-23 08:37:43 -0800

    load modules provider

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,8 +2,17 @@
 
 namespace Humweb\Tests\Modules;
 
+use Humweb\Modules\ModuleServiceProvider;
 
 class TestCase extends \Orchestra\Testbench\TestCase
 {
+
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            ModuleServiceProvider::class
+        ];
+    }
 
 }
